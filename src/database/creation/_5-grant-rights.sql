4e24427103265f0ef328de6f8b39d0f33a387bd3
nuc134r <chelman.soft@gmail.com>
2016-09-04 22:41:28 +0300

    Design

--- src/database/creation/_5-grant-rights.sql
+++
@@ -1,5 +0,0 @@
-GRANT ALL ON SCHEMA public TO postgres;
-GRANT ALL ON SCHEMA public TO public;
-GRANT ALL ON SCHEMA public TO test_user;
-
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO test_user; 
