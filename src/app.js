52d91345f4b9a124582d9fb00e672973205f89fd
nuc134r <chelman.soft@gmail.com>
2016-09-15 00:03:59 +0300

    Refactoring

--- src/app.js
+++ src/app.js
@@ -3,42 +3,43 @@
 let path = require('path');
 
 /* express */
-let express = require('express');
-let favicon = require('serve-favicon');
-let logger = require('morgan');
+let express      = require('express');
+let favicon	     = require('serve-favicon');
+let logger       = require('morgan');
 let cookieParser = require('cookie-parser');
-let bodyParser = require('body-parser');
+let bodyParser   = require('body-parser');
 
-/* database */
+/* mongo */
 //let mongo = require('mongodb');
 //let monk = require('monk');
 //let db = monk('localhost:27017/schoolportal');
 
-/* app */
 let app = express();
 
+/* jade */
 app.set('view engine', 'jade');
 app.set('views', 'src/views');
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+
+/* util */
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 /* static resources */
+//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(express.static(path.join(__dirname, 'public/css')));
 app.use(express.static(path.join(__dirname, 'public/scripts')));
 app.use(express.static(path.join(__dirname, 'public/assets')));
-app.use(express.static(path.join(__dirname, 'public/fonts')));
 
 /* auth */
 //let Authenticator = 
 //app.use(authenticator);
 
 /* repositories */
-let Repository = require('./repository/repository');
-let repository = new Repository(/* db */);
-app.use(repository);
+let RepoMiddleware = require('./repository/repository');
+let repoMiddleware = new RepoMiddleware(/* db */);
+app.use(repoMiddleware);
 
 /* routes */
 app.use('/', require('./routes/login-route'));
--- src/routes/login-route.js
+++ src/routes/login-route.js
@@ -1,11 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-// GET /
+// GET /login
 router.get('/login', function (req, res) {
-    
-    
-
     res.render('login', arguments);
     res.end();
 });
