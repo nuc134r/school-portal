0066088ef3ff3c560dfb9bca3099abe631ab7b9e
Sergey Tikhonov <Sergey Tikhonov>
2016-11-04 17:24:10 +0300

    dfg

--- public/scripts/common.js
+++ public/scripts/common.js
@@ -1,17 +1,33 @@
 var $content;
+var hasNavigated = false;
 
 $(document).ready(function () {
     $content = $('#content');
+
+    History.Adapter.bind(window, 'statechange', function () {
+        if (hasNavigated) {
+            var state = History.getState();
+            ajax(state.url);
+        } else {
+            hasNavigated = true;
+        }
+    });
 });
 
-function ajax(link) {
+function ajax(link, closeDrawer) {
 
-    var layout = document.querySelector('.mdl-layout');
-    layout.MaterialLayout.toggleDrawer();
+    if (closeDrawer) {
+        var layout = document.querySelector('.mdl-layout');
+        layout.MaterialLayout.toggleDrawer();
+    }
 
     $.ajax({ url: link.href, data: { 'ajax': 1 } })
         .done(function (data) {
             $content.html(data);
+            History.pushState(link.href, 'Keker', link.href);
+            if (!(typeof (componentHandler) == 'undefined')) {
+                componentHandler.upgradeAllRegistered();
+            }
         })
         .error(function (error) {
             alert(error);
--- src/validator.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-const joi = require('joi');
-
