c43af98dc1ccb89f7f1e3970b880dd9729a975a7
nuc134r <chelman.soft@gmail.com>
2016-12-19 23:38:38 +0300

    Hours in timeleft

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,9 @@
 'use strict';
 
 module.exports = {
-    
+
     capitalize: (s) => s.charAt(0).toUpperCase() + s.slice(1),
-    
+
     locateLessonInTime: (lesson) => {
         let dateNow = new Date;
         let now = dateNow.getHours() * 60 + dateNow.getMinutes();
@@ -11,13 +11,19 @@ module.exports = {
         let begins = lesson.timing.start.getHours() * 60 + lesson.timing.start.getMinutes();
         let ends = begins + lesson.timing.duration;
 
-        let isNow = begins <= now && now < ends; 
+        let isNow = begins <= now && now < ends;
+
+        let minutesLeft = ends - now;
+        let hoursLeft = Math.floor(minutesLeft / 60);
+        let timeleftString = hoursLeft
+            ? `${hoursLeft} ч ${minutesLeft - (60 * hoursLeft)} мин`
+            : `${minutesLeft} мин`;
 
         return {
             begins,
             ends,
             isNow,
-            timeleftString: isNow ? `осталось ${ends - now} мин` : null
+            timeleftString
         }
     }
 }
