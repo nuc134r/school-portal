a806f123da686cd9d289f583808569bd44d943c3
nuc134r <chelman.soft@gmail.com>
2017-05-14 03:19:57 +0300

    messages page

---
+++ src/database/raw-queries.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports.getAllChats = `
+
+SELECT 
+	* 
+FROM
+(
+	SELECT 
+		CASE
+			WHEN "fromId" = 11 THEN "toId"
+			WHEN "toId" = 11 THEN "fromId"
+		END AS "chatWithUserId",
+		"fromId" AS "lastMessagefromId",
+		"text",
+		"createdAt" AS "messageCreatedAt"
+	FROM
+	    (
+		SELECT 
+		    MAX(id) as "lastMessageId",
+		    CASE
+			WHEN "fromId" <= "toId" THEN ("fromId"::text || '.' || "toId"::text)
+			WHEN "fromId" > "toId" THEN ("toId"::text || '.' || "fromId"::text)
+		    END AS "ChatId"
+		FROM 
+		    public.messages
+		WHERE
+		    "fromId" = 11 
+		    OR 
+		    "toId" = 11
+		GROUP BY "ChatId"
+	    ) AS "chats"
+
+	JOIN  public.messages ON id = "lastMessageId"
+) AS "chats"
+
+JOIN public.users ON id = "chatWithUserId"
+ORDER BY "messageCreatedAt" DESC
+
+`;
